9606748b6e07aeb699f064d7eec981d7520e1a72
Alexandria <engineerpearl0@engineerpearl0-HP-Pavilion-Laptop-15-cc0xx>
2018-03-13 17:10:40 -0700

    Update .R file to have sections

---
+++ CrashReport.R
@@ -0,0 +1,26 @@
+#Gatheringd
+###alexandria read local:
+###israel read local: 
+
+##title:  “Crash Reporting - Drivers Data”
+data = read.csv("https://raw.githubusercontent.com/IsraelAndrade22/MontgomeryCountyCrashData/master/Crash_Reporting_-_Drivers_Data.csv")
+#Initial Exploration:
+
+##Do some initial exploration of the data.  How many rows and columns?  What are the types of the columns (ints, strings, factors, …?).  Don’t forget the ‘summary’ and ‘str’ commands.
+
+#cleaning
+
+
+
+#exploration and visualization
+
+## NA, or corrupted data?  Identify and treat them appropriately.
+
+##    what are the most common kinds of crashes?
+##are certain kinds of crashes more common at certain times and places?
+##  are some kinds of crashes more common on certain kinds of roads?
+  
+  
+#conclusions
+
+#Questions from exploration
