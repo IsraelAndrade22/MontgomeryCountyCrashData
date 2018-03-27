6bd543365e6eedaaaae65156427d173dd1d04432
Alexandria <engineerpearl0@engineerpearl0-HP-Pavilion-Laptop-15-cc0xx>
2018-03-26 17:12:58 -0700

    Modify files i guess

--- CrashReport.R
+++ CrashReport.R
@@ -13,21 +13,44 @@ plot(data$Weather, horiz = TRUE, las = 2)
 unknown = data[data$Weather == 'UNKNOWN',]
 bad = data[data$Weather == 'N/A',]
 
-countna = function(x){
+countbadula = function(x){
   total = sum(is.na(x))
-  total %+=% sum(x == "N/A")
-  total %+=% sum(x == "")
-  total
+  total = total + sum(x == "N/A", na.rm = TRUE)
+  total = total + sum(x == "", na.rm = TRUE)
+  total/sum(lengths(x))
 }
-
-
+isblank = function(x) {
+  mean(x=="", na.rm = TRUE)
+}
+countna = function(x) {
+  mean(x=="", na.rm = TRUE)
+}
 #cleaning
 
 
 #exploration and visualization
+# 
+  ####The heck is an autocycle
+sort(table(data$Vehicle.Body.Type))# 1. the heck is an autocycle? 2. why so many emergency vehicles?
+####Why is "Driverless moving vehicles" a movement type???
+  ####pinpoint and identify dangerous stoplights
+
+plot(data$Latitude, data$Longitude, xlim = c(38.7, 39.4), ylim = c(-78, -76.5))
+#Find the intersectins.
+#Also there's a person in our data
+corner = data[data$Vehicle.Movement == c("MAKING LEFT TURN", "MAKING RIGHT TURN", "RIGHT TURN ON RED"),]
+plot(corner$Latitude, corner$Longitude, xlim = c(38.9, 39.5), ylim = c(-77.6, -76.7))
+
+#Agencies acording to how often the moterist is at fault
+par(las = 2, mar = c(4,4,4,4))
+plot(data$Agency.Name, data$Driver.At.Fault)
+#collision types based on driver at fault
+par(las = 2, mar = c(8,5,2,2), cex = 0.7)
+plot(data$Collision.Type, data$Driver.At.Fault)
 
 ## NA, or corrupted data?  Identify and treat them appropriately.
-
+  ####identify if Driver.substance.abuse goes in unknown or none
+  ####
 ##    what are the most common kinds of crashes?
 ##are certain kinds of crashes more common at certain times and places?
 ##  are some kinds of crashes more common on certain kinds of roads?
